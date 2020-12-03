36996db9a58dc25720c44c5793538dee53e26d7a
Kostas <vrinek@hey.com>
2020-12-03 21:53:56 +0100

    Extract appending a map to a file to its own fn
    And spruce up the docstrings a little bit

--- src/mech_keys_scrape/core.clj
+++ src/mech_keys_scrape/core.clj
@@ -21,11 +21,12 @@
     (filter #(re-find #"/products/" (get-element-attr-el driver % :href)) all-links-under-products)))
 
 (defn get-current-price
-  "Return the current price of the product on the page"
+  "Return the price of the product on the page"
   [driver]
-  (get-element-text driver {:css ".current_price .money"}))
+  (get-element-text driver {:css ".modal_price"}))
 
 (defn get-current-product-name
+  "Return the product name on the page"
   [driver]
   (get-element-text driver {:css "h1.product_name"}))
 
@@ -47,27 +48,28 @@
    (query-all driver {:css ".swatch_options label"})))
 
 (defn visit-all-variants
-  "Visits each variant of the current product"
-  [driver visited-variants]
+  "Visits each variant of the current product and apply fn to them"
+  [driver product-map fn visited-variants]
   (let [all-variants (get-variant-labels driver)
         unvisited-variants (filter #(not (visited-variants %)) all-variants)]
     (if-let [target-variant (first unvisited-variants)]
       (do
         (click-el driver target-variant)
-        (println (get-element-text-el driver target-variant))
-        (println (get-element-text driver {:css ".modal_price"}))
-        (recur driver (conj visited-variants target-variant))))))
+        (let [variant-map {:variant (get-element-text-el driver target-variant)
+                           :price (get-current-price driver)}
+              merged-map (merge product-map variant-map)]
+          (fn merged-map))
+        (recur driver product-map fn (conj visited-variants target-variant))))))
 
 (defn visit-product
-  "Visits the page of a product and prints its details"
-  [driver product-link]
+  "Visits the page of a product and its variants"
+  [driver product-link fn]
   (click-el driver product-link)
   (let [product-map {:name (get-current-product-name driver)
                      :price (get-current-price driver)
                      :url (get-url driver)
                      :last-updated (java.util.Date.)}]
-    (println product-map)
-    (visit-all-variants driver (hash-set))
+    (visit-all-variants driver product-map fn (hash-set))
     product-map))
 
 (defn filter-unvisited-links
@@ -75,22 +77,26 @@
   (filter #(not (visited-href (link->href driver %))) links))
 
 (defn visit-all-products
-  "Recursively visits all products"
-  ([driver]
-   (visit-all-products driver (hash-set)))
-  ([driver visited-href]
+  "Recursively visits all products and apply fn to them"
+  ([driver fn]
+   (visit-all-products driver fn (hash-set)))
+  ([driver fn visited-href]
   (let [all-links (get-product-links driver)
         unvisited-links (filter-unvisited-links driver visited-href all-links)]
     (if-let [target-link (first unvisited-links)]
       (let [target-href (link->href driver target-link)
-            product-map (visit-product driver target-link)]
-        (spit output-file (str (generate-string product-map) "\n") :append true)
-        (recur driver (conj visited-href target-href)))))))
+            product-map (visit-product driver target-link fn)]
+        (recur driver fn (conj visited-href target-href)))))))
+
+(defn append-map-as-json
+  "Transform a map to JSON and append it to a file"
+  [file map]
+  (spit file (str (generate-string map) "\n") :append true))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [driver (firefox)]
     (go driver "https://zealpc.net/")
-    (visit-all-products driver)
+    (visit-all-products driver (partial append-map-as-json output-file))
     (quit driver)))
